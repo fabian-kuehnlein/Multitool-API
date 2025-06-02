f10af0593fd5a69616e3fad6e945c19b27422593
Fabian Kühnlein <fabian.kuehnlein@outlook.com>
2025-06-02 15:56:17 +0200

    Added recurring events to Get-Route

--- Infrastructure.DataAccessLayer/Models/CalendarEventDAO.cs
+++ Infrastructure.DataAccessLayer/Models/CalendarEventDAO.cs
@@ -9,4 +9,5 @@ public class CalendarEventDAO
     public DateTime? EndDateTime { get; set; }
     public required bool IsAllDay { get; set; }
     public required int CategoryId { get; set; }
-}
+    public string? RecurrenceRule { get; set; }
+    public DateTime? RecurrenceEnd { get; set; }}
