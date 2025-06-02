588607fe1574794b8fd85ea2743cf8b03ea20d78
Fabian Kuehnlein <fabian.kuehnlein@outlook.com>
2025-06-02 15:56:17 +0200

    Added recurring events to Get-Route

--- Infrastructure.WebApi/Models/CalendarEventDTO.cs
+++ Infrastructure.WebApi/Models/CalendarEventDTO.cs
@@ -9,4 +9,6 @@ public class CalendarEventDTO
     public DateTime? EndDateTime { get; set; }
     public required bool IsAllDay { get; set; }
     public required int CategoryId { get; set; }
+    public string? RecurrenceRule { get; set; }
+    public DateTime? RecurrenceEnd { get; set; }
 }
