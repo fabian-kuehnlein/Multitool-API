8d9d4ba9c3e7000b33b961ae3dbd7687a68b386e
Fabian Kühnlein <fabian.kuehnlein@outlook.com>
2026-01-07 20:22:39 +0100

    Added Color to the category table for the background color of calendar events

--- Infrastructure.DataAccessLayer/Models/calendar/Category.cs
+++ Infrastructure.DataAccessLayer/Models/calendar/Category.cs
@@ -4,4 +4,5 @@ public class Category
 {
     public required int CategoryId { get; set; }
     public required string CategoryName { get; set; }
+    public required string Color { get; set; }
 }
---
+++ Migrations/20260107192123_AddCategoryColors.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MultitoolApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryColors : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "color",
+                table: "categories",
+                type: "varchar(9)",
+                maxLength: 9,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "color",
+                table: "categories");
+        }
+    }
+}
