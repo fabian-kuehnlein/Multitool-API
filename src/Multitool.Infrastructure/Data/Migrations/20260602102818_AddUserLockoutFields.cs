af65b9f1f4d4c515a8c01b23784a0c850573f5ee
Fabian Kühnlein <fabian.kuehnlein@outlook.com>
2026-06-02 12:29:14 +0200

    fix(auth): add missing user lockout migration

---
+++ src/Multitool.Infrastructure/Data/Migrations/20260602102818_AddUserLockoutFields.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Multitool.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserLockoutFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "access_failed_count",
+                table: "users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "lockout_end",
+                table: "users",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "access_failed_count",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "lockout_end",
+                table: "users");
+        }
+    }
+}
